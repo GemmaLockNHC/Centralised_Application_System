4cb0a0507559a4b5c3df2131575f156c572dd988
GemmaLockNHC <gemma.lock@nhcare.com.au>
2025-04-07 13:50:49 +0800

    Updated 'server.js'.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 8000;
+
+// Use the environment variable for the port or default to 8000 if running locally
+const PORT = process.env.PORT || 8000;
 
 // Serve static files (images, CSS, JS) under /static
 app.use('/static', express.static(path.join(__dirname, 'public')));
@@ -11,11 +13,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Centralised_System.html'));
 });
 
+// If you have a mobile version or another route, add it here
+app.get('/mobile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'Centralised_System_Mobile.html'));
+});
+
 // Block any other routes
 app.use((req, res) => {
   res.status(403).send('Forbidden');
 });
 
+// Listen on the port specified by Render or 8000 locally
 app.listen(PORT, () => {
   console.log(`Secure server running at http://localhost:${PORT}`);
 });
