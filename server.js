8358cf04843ae15fb0a5c24b4735621c216c5634
GemmaLockNHC <gemma.lock@nhcare.com.au>
2025-04-07 14:47:54 +0800

    Updated 'server.js'.
    Corrected the message to the console to indicate that the server is running successfully. Removed unnecessary comments.

--- server.js
+++ server.js
@@ -8,15 +8,10 @@ const PORT = process.env.PORT || 8000;
 // Serve static files (images, CSS, JS) under /static
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-// Serve ONLY index.html at root
+// Serve ONLY Centralised_System.html at root
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Centralised_System.html'));
 });
-
-// If you have a mobile version or another route, add it here
-//app.get('/mobile', (req, res) => {
-//  res.sendFile(path.join(__dirname, 'public', 'Centralised_System_Mobile.html'));
-//});
 
 // Block any other routes
 app.use((req, res) => {
@@ -25,5 +20,5 @@ app.use((req, res) => {
 
 // Listen on the port specified by Render or 8000 locally
 app.listen(PORT, () => {
-  console.log(`Secure server running at http://localhost:${PORT}`);
+  console.log('Server running successfully!');
 });
